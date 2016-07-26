b95e67acd70f296d3935399902543897ff19902f
ga-wolf <jack.jeffress@ga.co>
2016-07-26 11:58:15 +1000

    Removed comments

--- 01-javascript/primitives_and_variables/main.js
+++ 01-javascript/primitives_and_variables/main.js
@@ -1,39 +1,39 @@
-// // Primitive Data Types
-// // Strings
-//
-// console.log( "Hello World" );
-// console.log( "It's a bag" );
-// console.log( "The person says \"hi\"" );
-//
-// // Numbers
-//
-// console.log( 42 );
-// console.log( 1.2 );
-// console.log( -1000 );
-// console.log( 4 + 2 );
-// console.log( 5 - 2 );
-// console.log( 4 * 2 ); // Multiplication
-// console.log( 3 / 6 ); // Division
-// console.log( 5 % 4 ); // The modulus operator (remainder)
-//
-// // Booleans
-//
-// console.log( true );
-// console.log( false );
-//
-// // Undefined
-//   // to be used by the browser
-//
-// console.log( undefined );
-//
-// // Null
-//   // to be used by a programmer
-//
-// console.log( null );
-//
+// Primitive Data Types
+// Strings
 
+console.log( "Hello World" );
+console.log( "It's a bag" );
+console.log( "The person says \"hi\"" );
 
-// Variables
+// Numbers
+
+console.log( 42 );
+console.log( 1.2 );
+console.log( -1000 );
+console.log( 4 + 2 );
+console.log( 5 - 2 );
+console.log( 4 * 2 ); // Multiplication
+console.log( 3 / 6 ); // Division
+console.log( 5 % 4 ); // The modulus operator (remainder)
+
+// Booleans
+
+console.log( true );
+console.log( false );
+
+// Undefined
+  // to be used by the browser
+
+console.log( undefined );
+
+// Null
+  // to be used by a programmer
+
+console.log( null );
+
+///////////////
+// Variables //
+///////////////
 
 var x = 10;
 console.log("The data contained in the variable x is:", x);
