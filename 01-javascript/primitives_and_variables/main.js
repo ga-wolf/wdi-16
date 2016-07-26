ec9d57a478f34d3a5d2062d5f6745e461822ab8d
ga-wolf <jack.jeffress@ga.co>
2016-07-26 10:24:51 +1000

    Added primitive values

---
+++ 01-javascript/primitives_and_variables/main.js
@@ -0,0 +1,32 @@
+// Primitive Data Types
+// Strings
+
+console.log( "Hello World" );
+console.log( "It's a bag" );
+console.log( "The person says \"hi\"" );
+
+// Numbers
+
+console.log( 42 );
+console.log( 1.2 );
+console.log( -1000 );
+console.log( 4 + 2 );
+console.log( 5 - 2 );
+console.log( 4 * 2 ); // Multiplication
+console.log( 3 / 6 ); // Division
+console.log( 5 % 4 ); // The modulus operator (remainder)
+
+// Booleans
+
+console.log( true );
+console.log( false );
+
+// Undefined
+  // to be used by the browser
+
+console.log( undefined );
+
+// Null
+  // to be used by a programmer
+
+console.log( null );
