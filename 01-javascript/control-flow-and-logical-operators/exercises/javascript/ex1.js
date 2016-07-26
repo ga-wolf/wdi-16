e898eb178bd01579afe90ac151c1135fe598976d
Christopher Barnaby <cjbarnaby@gmail.com>
2016-07-26 17:23:46 +1000

    control flow exercises

---
+++ 01-javascript/control-flow-and-logical-operators/exercises/javascript/ex1.js
@@ -0,0 +1,14 @@
+// What number's bigger?
+//
+// Write an if statement that tests two number (numOne and numTwo), and prints the biggest number
+
+var numOne = 8;
+var numTwo = 8;
+
+if (numOne > numTwo) {
+    console.log(numOne);
+} else if (numOne < numTwo){
+    console.log(numTwo);
+} else {
+    console.log("They're the same number");
+}
---
+++ 01-javascript/control-flow-and-logical-operators/exercises/javascript/ex3.js
@@ -0,0 +1,18 @@
+// The Grade Assigner
+//
+// Write an if statement that gives users a score (A, B, C, D, or F) based on the test results
+
+var testResult = 80;
+
+
+if (testResult > 90 ) {
+    console.log("A");
+} else if (testResult > 80) {
+    console.log("B");
+} else if (testResult > 70) {
+    console.log("C");
+} else if (testResult > 60) {
+    console.log("D");
+} else {
+    console.log("F");
+}
