2c3895aa4b58781f59ef7f61d5ccd73b24725a41
ga-wolf <jack.jeffress@ga.co>
2016-08-18 17:37:44 +1000

    Finished SQL

--- 07_sql/animals/main.rb
+++ 07_sql/animals/main.rb
@@ -1,18 +1,18 @@
-require 'sqlite3'
-require 'pry'
-require 'sinatra'
-require 'sinatra/reloader'
+require 'sqlite3' # Provides an interface for Ruby to be able to speak to the sqlite3 command in your terminal
+require 'pry' # Debugging
+require 'sinatra' # Web Server
+require 'sinatra/reloader' # Don't restart the server every change we make
 
-get '/' do
+get '/' do # localhost:4567/
   erb :home
 end
 
-get '/pry' do
+get '/pry' do # Not necessary, but useful for debugging
   binding.pry
   "Pry route"
 end
 
-get '/animals' do
+get '/animals' do # localhost:4567/animals INDEX
   db = SQLite3::Database.new("database.db")
   db.results_as_hash = true
   @all_animals = db.execute "SELECT * FROM animals;"
@@ -26,9 +26,10 @@ get '/animals/new' do
 end
 
 post '/animals' do
+  sql = "INSERT INTO animals (species, image, description) VALUES ('#{params[:species]}', '#{params[:image]}', '#{params[:description]}');"
+
   db = SQLite3::Database.new("database.db")
   db.results_as_hash = true
-  sql = "INSERT INTO animals (species, image, description) VALUES ('#{params[:species]}', '#{params[:image]}', '#{params[:description]}');"
   db.execute sql
   db.close
 
@@ -47,6 +48,12 @@ end
 
 post '/animals/:id' do
   sql = "UPDATE animals SET species = '#{params[:species]}', image = '#{params[:image]}', description = '#{params[:description]}' WHERE id == #{params[:id]};"
+
+  puts "\n\n"
+  p params
+  puts "\n"
+  p sql
+  puts "\n\n"
 
   db = SQLite3::Database.new("database.db")
   db.results_as_hash = true
